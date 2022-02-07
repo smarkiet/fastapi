8652307a8fc893f4533cf1445db05a2f2d1801cf
Stephan Markiet <s.markiet@ad.euphoria-it.nl>
2022-02-07 19:35:19 +0100

    initial commit

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from . import models
+from .config import settings
+from .database import engine
+from .routers import auth, post, user, votes
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(votes.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello world"}
